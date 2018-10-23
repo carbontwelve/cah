ffaa6add7f0930e11b0e1b698e43255d030996b0
Simon Dann <simon.dann@achiive.co.uk>
2018-10-23 13:33:55 +0100

    :tada: Initial Commit

---
+++ database/migrations/2018_10_23_114515_create_cards_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCardsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cards', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+
+            $table->unsignedInteger('deck_id');
+            $table->text('content');
+            $table->enum('type', ['black', 'white']);
+            $table->integer('pick')->default(0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cards');
+    }
+}
---
+++ readme.md
@@ -0,0 +1,8 @@
+# Work in Progress Vue/Slack CaH Service
+
+Essentially a REST service for supplying cards for a game of CaH.
+
+## Todo
+
++ [x] Seed Official Cards
++ [ ] Complete todo list...
